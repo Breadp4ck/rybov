7da13978eb320c93da91517425d1f5e0b5c47ee2
TheActualTwinkle <super.irums2@gmail.com>
2024-04-03 23:30:33 +0700

    Фича #22

---
+++ Assets/Scripts/Units/Dragging/MouseFishDragger.cs
@@ -0,0 +1,88 @@
+﻿using System.Linq;
+using Inputs;
+using UnityEngine;
+using Zenject;
+
+namespace Units.Dragging
+{
+    [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+    public class MouseFishDragger : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+
+        [Header("Range")] [Range(0.1f, 2f)] [SerializeField]
+        private float _radius;
+        
+        private IInputSystem _inputSystem;
+
+        private IDraggable _draggable;
+
+        [Inject]
+        private void Construct(IInputSystem inputSystem)
+        {
+            _inputSystem = inputSystem;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, _radius);
+        }
+
+        private void Update()
+        {
+            if (_inputSystem.IsActionDown(InputAction.RightClick) == true)
+            {
+                IDraggable draggable = GetOverlappingDraggable();
+
+                if (draggable == null)
+                {
+                    return;
+                }
+                
+                draggable.StartDrag(transform);
+                _draggable = draggable;
+            }
+
+            if (_inputSystem.IsActionUp(InputAction.RightClick) == true)
+            {
+                if (_draggable != null)
+                {
+                    _draggable.StopDrag();
+                }
+                
+                _draggable = null;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+        private IDraggable GetOverlappingDraggable()
+        {
+            Collider2D[] overlappedColliders = new Collider2D[10];
+            int count = Physics2D.OverlapCircleNonAlloc(transform.position, _radius, overlappedColliders);
+
+            if (count == 0)
+            {
+                return null;
+            }
+            
+            overlappedColliders = overlappedColliders.Where(x => x != null).ToArray();
+            
+            foreach (Collider2D overlapped in overlappedColliders)
+            {
+                if (overlapped.TryGetComponent(out IDraggable draggable) == false)
+                {
+                    continue;
+                }
+
+                return draggable;
+            }
+
+            return null;
+        }
+    }
+}
--- Assets/Scripts/Units/StealableFish.cs
+++ Assets/Scripts/Units/StealableFish.cs
@@ -1,4 +1,7 @@
-﻿using Units.Destroying;
+﻿using System;
+using System.Collections;
+using Units.Destroying;
+using Units.Dragging;
 using Units.Movement.Fish;
 using Units.Spawning;
 using UnityEngine;
@@ -8,7 +11,7 @@ namespace Units
     /// <summary>
     /// Fish that registers in FishPool.cs
     /// </summary>
-    public class StealableFish : MonoBehaviour, IOutOfBorderInteractable
+    public class StealableFish : MonoBehaviour, IOutOfBorderInteractable, IDraggable
     {
         /// <summary>
         /// The one who stole and carries this fish at the moment.
@@ -16,7 +19,11 @@ namespace Units
         public IFishThief Thief;
 
         [SerializeField] private FishMovementStateMachine _stateMachine;
-        
+
+        private IEnumerator _followDragTransformRoutine;
+
+        #region IOutOfBorderInteractable
+
         public void OnSteal(IFishThief thief)
         {
             Thief = thief;
@@ -40,5 +47,77 @@ namespace Units
             FishPool.OutOfBorder(this);
             Destroy(gameObject);
         }
+
+        #endregion
+
+        #region IDraggable
+
+        public void StartDrag(Transform followTransform)
+        {
+            if (Thief != null)
+            {
+                Debug.LogError("Fish is already stolen. Can't drag it.");
+                return;
+            }
+
+            _stateMachine.TryChangeState<CarriedState>();
+
+            if (_followDragTransformRoutine != null)
+            {
+                StopCoroutine(_followDragTransformRoutine);
+            }
+
+            _followDragTransformRoutine = FollowDragTransform(followTransform);
+            StartCoroutine(_followDragTransformRoutine);
+        }
+
+        public void StopDrag()
+        {
+            _stateMachine.TryChangeState<FidgetingState>();
+            
+            if (_followDragTransformRoutine != null)
+            {
+                StopCoroutine(_followDragTransformRoutine);
+            }
+        }
+        
+        private IEnumerator FollowDragTransform(Transform followTransform)
+        {
+            const float lerpSpeed = 0.1f; // Скорость интерполяции. Можно настроить по своему усмотрению.
+            const float minDistanceForRotation = 0.01f; // Минимальное расстояние для поворота. Можно настроить по своему усмотрению.
+            const float rotationSpeed = 5f; // Скорость вращения. Можно настроить по своему усмотрению.
+
+            Vector2 previousPosition = transform.position;
+
+            while (true)
+            {
+                // Используем Lerp для плавного перемещения объекта к позиции followTransform.
+                Vector2 newPosition = Vector2.Lerp(transform.position, followTransform.position, lerpSpeed);
+                transform.position = newPosition;
+
+                // Вычисляем угол между старым и новым направлением движения только если пройдено достаточное расстояние.
+                if (Vector2.Distance(newPosition, previousPosition) >= minDistanceForRotation)
+                {
+                    Vector2 oldDirection = transform.up;
+                    Vector2 newDirection = newPosition - previousPosition;
+                    float angle = Vector2.SignedAngle(oldDirection, newDirection);
+
+                    // Ограничиваем угол поворота относительно горизонта.
+                    angle = Mathf.Clamp(angle, -45, 45);
+
+                    // Поворачиваем объект на вычисленный угол.
+                    Quaternion currentRotation = transform.rotation;
+                    Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+                    transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, Time.deltaTime * rotationSpeed);
+                }
+
+                previousPosition = newPosition;
+
+                yield return null;
+            }
+            // ReSharper disable once IteratorNeverReturns
+        }
+        
+        #endregion
     }
 }

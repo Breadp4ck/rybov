7c8c34c1e7f8cd6a8e95b2af9877c45abc46efd5
TheActualTwinkle <super.irums2@gmail.com>
2024-04-07 17:51:35 +0700

    Фича #25. Добавил: SampleScene для копирования и билда уровней

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,3 +1,17 @@
-﻿public static class Constants
+﻿using System.Collections.Generic;
+using GlobalState.Level;
+
+public static class Constants
 {
+    public enum SceneType : byte
+    {
+        MainMenu,
+        Level,
+    }
+
+    public static Dictionary<SceneType, string> Scenes = new()
+    {
+        { SceneType.MainMenu, "MainMenu"},
+        { SceneType.Level, $"Level_{Level.Instance.NextLevelIndex}"},
+    };
 }

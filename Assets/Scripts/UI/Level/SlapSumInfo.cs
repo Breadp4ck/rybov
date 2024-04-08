9f16c1f23263f452e6e7e42cc521ee900a81abd6
Ivan Danchenko <danchenko.ivan@inbox.ru>
2024-04-09 04:23:29 +0700

    Версия 1.01

---
+++ Assets/Scripts/UI/Level/SlapSumInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GlobalState.Scores;
+using GlobalStates.Game;
+using TMPro;
+using Units;
+using UnityEngine;
+
+namespace UI.Level
+{
+    public class SlapSumInfo : MonoBehaviour
+    {
+        private Scores Scores => Scores.Instance;
+
+        [SerializeField] private TextMeshProUGUI _text;
+
+        private void Update()
+        {
+            _text.text = $"{Scores.HitsScore.Sum(x => x.Value)}";
+        }   
+    }
+}

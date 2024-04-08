5baf1dec4db8d075cc873f5cfb3a824e0a15a295
Ivan Danchenko <danchenko.ivan@inbox.ru>
2024-04-09 00:37:50 +0700

    Sounds to main menu

---
+++ Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,18 @@
+﻿using SceneManagement.SceneManagement;
+using UnityEngine;
+
+namespace UI.MainMenu
+{
+    public class MainMenuUI : MonoBehaviour
+    {
+        public async void OnPlayButtonClicked()
+        {
+            await SceneChanger.ChangeSceneAsync(Constants.SceneType.Level0);
+        }
+
+        public void OnExitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu/SoundPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UI.MainMenu
+{
+    public class SoundPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+
+        public void PlaySound()
+        {
+            _audioSource.Play();
+        }
+    }
+}
